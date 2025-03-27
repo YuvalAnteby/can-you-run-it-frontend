23b1d64acb6a98811149b89de7e70e10899c5817
yuval <yuvala2001@gmail.com>
2025-03-27 19:28:14 +0200

    Created component to select game requirements

--- src/components/RequirementsSelection.js
+++ src/components/RequirementsSelection.js
@@ -1,6 +1,54 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {Box, FormControl, InputLabel, MenuItem, Select} from "@mui/material";
+import FpsSlider from "../components/FpsSlider";
 
-const RequirementsSelection = () => {
+const RequirementsSelection = ({game, resolution, setResolution, setting, setSetting, fps, setFps}) => {
 
+
+    useEffect(() => {
+        console.log('Game:', game);
+    }, [game]);
+
+    return (
+        <Box sx={{ padding: '20px' }}>
+            <FormControl fullWidth variant="filled" margin="normal"
+                         style={{padding: '0px', marginTop: '10px'}}>
+
+                <InputLabel>Resolution</InputLabel>
+                <Select
+                    sx={{textAlign: "left"}}
+                    value={resolution}
+                    // Update the selected company
+                    onChange={(e) => {
+                        setResolution(e.target.value);
+                    }}
+                    label={`Resolution`}>
+                    {game.available_resolutions.map((resolution) => (
+                        <MenuItem key={resolution} value={resolution}>{resolution}</MenuItem>
+                    ))}
+                </Select>
+            </FormControl>
+
+            <FormControl fullWidth variant="filled" margin="normal"
+                         style={{padding: '0px', marginTop: '10px'}}>
+
+                <InputLabel>Graphic level</InputLabel>
+                <Select
+                    sx={{textAlign: "left"}}
+                    value={setting}
+                    // Update the selected company
+                    onChange={(e) => {
+                        setSetting(e.target.value);
+                    }}
+                    label={`Graphic level`}>
+                    {game.supported_settings.map((setting_name) => (
+                        <MenuItem key={setting_name} value={setting_name}>{setting_name}</MenuItem>
+                    ))}
+                </Select>
+            </FormControl>
+            <FpsSlider minFps={0} maxFps={240} fps={fps} setFps={setFps}/>
+        </Box>
+    )
 
 }
+export default RequirementsSelection;
